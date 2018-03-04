9a3c640089c491510834b40ee1d501548b88091e
Eder Soares <edersoares@me.com>
2018-03-04 18:35:27 -0300

    Modified User model to use UUID

--- app/User.php
+++ app/User.php
@@ -2,12 +2,22 @@
 
 namespace Wallet;
 
+use Uuid;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Wallet\Support\UuidModel;
 
-class User extends Authenticatable
+class User extends UuidModel implements
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
 {
-    use Notifiable;
+    use Authenticatable, Authorizable, CanResetPassword, Notifiable;
 
     /**
      * The attributes that are mass assignable.
