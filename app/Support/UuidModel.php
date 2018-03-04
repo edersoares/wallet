c79ca9e01d5e8f09d09c0c0479a1b7fe494fb0f0
Eder Soares <edersoares@me.com>
2018-03-04 18:34:53 -0300

    Base model to use UUID

---
+++ app/Support/UuidModel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Wallet\Support;
+
+use Uuid;
+use Illuminate\Database\Eloquent\Model;
+
+class UuidModel extends Model
+{
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'uuid';
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($model) {
+            $model->uuid = (string) Uuid::generate(4);
+        });
+    }
+}
