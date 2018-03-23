06d42e504444b9084c30170e8acf9254e74e366b
Eder Soares <edersoares@me.com>
2018-03-23 19:54:52 -0300

    Remove background color from login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,6 @@
             justify-content: center;
             padding-top: 40px;
             padding-bottom: 40px;
-            background-color: #f5f5f5;
             min-height: 100%;
         }
 
